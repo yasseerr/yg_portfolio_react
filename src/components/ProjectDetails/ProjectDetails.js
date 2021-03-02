8dd725512be0ef1325bcd7d64a2ce7174cc2dbcf
yasseerr <y.grimes@esi-sba.dz>
2021-03-02 22:56:40 +0100

    designed the project details ui

--- src/components/ProjectDetails/ProjectDetails.js
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -4,14 +4,30 @@ import './ProjectDetails.scss';
 import { displayProject } from "../../actions";
 
 import { connect } from "react-redux";
+import { Icon } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+
+import logo from "src/data/VisualBlueprint/i01_colored.png";
+//import img1 from "data/VisualBlueprint/i01_colored.png";
 
 const ProjectDetails = (props) => {
-  const [counter, setCounter] = useState(0); 
+  const [imageIndex,setImageIndex] = useState(0);
+  const tick = ()=> setImageIndex(imageIndex+1);
+  const tock = ()=> setImageIndex(imageIndex-1);
   return(
     <div className="ProjectDetails">
-      ProjectDetails Component {counter}
-      <p> current project {props.current_project} </p>
-      <button onClick={()=>setCounter(counter+1)}>Click me</button>
+      <h3>Project: {props.current_project}</h3>
+      <div className="description">Description:
+      <p>the project was developed when you know something was missing and needed to be developed</p>
+      </div>
+      <div className="gallery">
+        <center style={{height: "100%",width: "100%"}}><img className="project-image" src={logo}></img></center>
+        <div className="bottom-menu">
+          <div onClick={tock} class="bottom-button"><center style={{height: "100%"}}><Icon className="bottom-icon" iconSize={24} color="white" icon="arrow-left"/></center></div>
+          <div onClick={tick} class="bottom-button"><center style={{height: "100%"}}><Icon className="bottom-icon" iconSize={24} color="white" icon="arrow-right"/></center></div>
+          <span style={{verticalAlign: "middle"}}>Image : {imageIndex}</span>
+        </div>
+      </div>
   </div>
   )
 };

3a6c4fcd493c1e277770f14637fa8eeb26eae044
yasseerr <y.grimes@esi-sba.dz>
2021-03-15 11:55:00 +0100

    configured the left menu items

--- src/components/MiddleMenu/MiddleMenu.js
+++ src/components/MiddleMenu/MiddleMenu.js
@@ -8,6 +8,12 @@ import { Icon } from "@blueprintjs/core";
 import { IconNames,IconSvgPaths20, IconContents } from "@blueprintjs/icons";
 import PlanetItem from '../PlanetItem/PlanetItem';
 
+///images for links
+import gmail_icon from "../../data/images/gmail2.png";
+import github_icon from "../../data/images/github2.png";
+import gitlab_icon from "../../data/images/gitlab2.png";
+import upwork_icon from "../../data/images/upwork2.png";
+
 const getRotationPositon = (i) => {
   let initial_angle = Math.PI * 0.95;
   let angle_step = Math.PI / 10;
@@ -22,14 +28,20 @@ function MenuItem(props) {
   let pos = getRotationPositon(props.index);
   return (
     <div className="menu-item" style={{ left: pos.a, top: pos.b }}>
-      <Icon className="menu-item-icon" icon={props.info.icon} iconSize={20}/>
+      <img className="menu-item-icon" src={props.info.icon} />
+      <a href={props.info.link} target="_blank" style={{height: "100%",width:"100%",position: "absolute",top: "0%",left:"0%"}}/>
     </div>
   )
 }
 
 
 const MiddleMenu = () => {
-  let items = [{icon: IconNames.GIT_REPO},{icon: IconNames.WALK},{icon: IconNames.LINK},{icon: IconNames.MEDIA}]
+  let items = [
+    {icon: gmail_icon, link: "mailto:y.grimes@esi.dz"},
+    {icon: github_icon, link: "https://www.github.com/yasseerr"},
+    { icon: gitlab_icon, link: "https://gitlab.com/yasseerr"},
+    { icon: upwork_icon, link: "https://www.upwork.com/freelancers/~01932ce8601f9efcf0j"}
+  ]
   const items_rendered = items.map((e,i) => <MenuItem index={i} info={e}/>);
 
   return(
